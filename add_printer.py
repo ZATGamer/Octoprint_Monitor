fcd328382f379fce4b461f310de9f3f7a9d79826
Anthony <gcnplayer5@gmail.com>
2020-05-11 22:27:06 -0600

    Script to add the printers to the database

---
+++ add_printer.py
@@ -0,0 +1,16 @@
+from print_stall import db_setup, db_setup_connect
+
+
+if __name__ == '__main__':
+    db_file = './stats.db'
+    conn = db_setup_connect()
+
+    ip = input("What is the Printers IP?: ")
+    number = input("What is the Printer Number?: ")
+    api_key = input("What is the API Key?: ")
+    data = (number, ip, api_key)
+    sql = """ INSERT INTO state (printer_number, printer_ip, printer_api_key) VALUES(?,?,?); """
+    cur = conn.cursor()
+    cur.execute(sql, data)
+    conn.commit()
+    conn.close()

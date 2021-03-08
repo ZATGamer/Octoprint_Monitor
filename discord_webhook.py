630e915c5eb2bb071e2419fa0597e65484ee0310
Anthony <gcnplayer5@gmail.com>
2021-03-08 13:00:25 -0700

    new discord messages

--- discord_webhook.py
+++ discord_webhook.py
@@ -1,20 +1,32 @@
 import requests
 
-def send_discord_message(subject, message):
+
+def send_discord_message(subject, message, printer):
     print("TEST")
     data = {
+        "username": "Printer {}".format(printer),
         "content": "{}, {}".format(subject, message),
-        "tts": False,
-        "embed": [{
-            "name": "Hello, Embed!",
-            "description": "This is an embedded message."
-        }]
+        #"color": 3553598,
+        # "embeds": [
+        #     {
+        #         "author": {
+        #             "name": "Captain Hook"
+        #         },
+        #         "title": "My new embed",
+        #         "description": "This is a cool-looking Discord embed, sent directly from JavaScript!",
+        #         "color": 3553598
+        #     },
+        # ],
+        "tts": False
     }
+    headers = {'Content-type': 'application/json'}
+
 
     webhook_base_url = "https://discord.com/api/webhooks/803386709735768144/V3zdiPK-wcaa-VcdEE_h9XcTFQkCEslD1UyE9U-DlwjjQPPHO_rGRkfxLp1XIBXXVtkY"
     print("Sending Discord Message")
     try:
-        requests.post(webhook_base_url, data=data)
+        test = requests.post(webhook_base_url, data=data)
+        print(test.status_code)
         return 1
     except:
         print("Something Went Wrong")
@@ -22,4 +34,4 @@ def send_discord_message(subject, message):
 
 
 if __name__ == '__main__':
-    send_discord_message("Test", "test")
+    send_discord_message("TestE", "teste", "T1")
